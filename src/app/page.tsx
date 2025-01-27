9e954ae79f6fae983ed65ed2a2e6c9b2030734a7
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-01-26 23:35:11 -0800

    added new route for adding generated challenge ideas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
-"use client";
-
+import { prisma } from "@/lib/db/(root)/prisma";
+import { getChallenges } from "@/lib/db/challenge";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
-export default function Page() {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
 
-    const fileInput = e.target.elements[0] as HTMLInputElement;
-    const file = fileInput.files?.[0];
+const Page = async () => {
+  const { userId } = await auth();
 
-    fetch("/api/upload-image", {
-      method: "POST",
-      body: JSON.stringify({
-        base64: await new Promise<string>((resolve) => {
-          const reader = new FileReader();
-          reader.onload = () => resolve(reader.result as string);
-          reader.readAsDataURL(file);
-        }),
-      }),
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  };
+  return <div>Page</div>;
+};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" />
-      <button type="submit" className="bg-blue-500 text-white p-3 rounded-xl">
-        Submit
-      </button>
-    </form>
-  );
-}
+export default Page;
---
+++ src/lib/db/challengeIdeas.ts
@@ -0,0 +1,3 @@
+export async function generateChallengeIdeas(challenges: any) {
+  return challenges;
+}
