01b704b682e0bb36afa6c5b33565093c831bd897
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-02-15 08:06:14 -0800

    refactor: moved pagelayout to layout folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,21 @@
-"use client";
+import { PageLayout } from "@/components/layout/PageLayout";
 
-import { ChallengeListGrid } from "@/components/ChallengeListGrid";
-import { CreateChallenge } from "@/components/molecule/challenge-form/CreateChallenge";
-import { ViewChallenge } from "@/components/ViewChallenge";
-import { trpc } from "@/lib/util/trpc";
-import { Loader2 } from "lucide-react";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-const Challenges = () => {
-  const { data: challenges, isLoading: isChallengesLoading } =
-    trpc.challenge.getChallenges.useQuery();
-
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-
-  const challengeId = searchParams.get("challenge");
-
-  useEffect(() => {
-    if (!isChallengesLoading && (!challenges || !challenges.length)) {
-      const params = new URLSearchParams(searchParams);
-      params.set("challenge", "new");
-      replace(`${pathname}?${params.toString()}`);
-    }
-  }, [isChallengesLoading, challenges, searchParams, pathname, replace]);
-
-  if (isChallengesLoading) {
-    return <Loader2 className="h-12 w-12 animate-spin" />;
-  }
-
-  if (challengeId === "new") {
-    return <CreateChallenge />;
-  }
-
-  if (!challengeId) {
-    return <ChallengeListGrid />;
-  }
-
-  return <ViewChallenge />;
+type GenerateMetadataProps = {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 };
 
+export async function generateMetadata({
+  searchParams,
+}: GenerateMetadataProps) {
+  const challenge = searchParams.challenge;
+
+  return {
+    title: challenge ? `Challenge: ${challenge}` : "Default Title",
+  };
+}
+
 export default function Page() {
-  const { data: user, isLoading } = trpc.user.getUser.useQuery();
-
-  if (!isLoading && !user) {
-    throw new Error("User not found");
-  }
-
-  return (
-    <>
-      <div className="my-6 flex flex-1 items-center justify-center">
-        <Challenges />
-      </div>
-    </>
-  );
+  return <PageLayout />;
 }
---
+++ src/components/layout/PageLayout.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { ChallengeListGrid } from "@/components/ChallengeListGrid";
+import { CreateChallenge } from "@/components/molecule/challenge-form/CreateChallenge";
+import { ViewChallenge } from "@/components/ViewChallenge";
+import { trpc } from "@/lib/util/trpc";
+import { Loader2 } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const Challenges = () => {
+  const { data: challenges, isLoading: isChallengesLoading } =
+    trpc.challenge.getChallenges.useQuery();
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const challengeId = searchParams.get("challenge");
+
+  useEffect(() => {
+    if (!isChallengesLoading && (!challenges || !challenges.length)) {
+      const params = new URLSearchParams(searchParams);
+      params.set("challenge", "new");
+      replace(`${pathname}?${params.toString()}`);
+    }
+  }, [isChallengesLoading, challenges, searchParams, pathname, replace]);
+
+  if (isChallengesLoading) {
+    return <Loader2 className="h-12 w-12 animate-spin" />;
+  }
+
+  if (challengeId === "new") {
+    return <CreateChallenge />;
+  }
+
+  if (!challengeId) {
+    return <ChallengeListGrid />;
+  }
+
+  return <ViewChallenge />;
+};
+
+export function PageLayout() {
+  const { data: user, isLoading } = trpc.user.getUser.useQuery();
+
+  if (!isLoading && !user) {
+    throw new Error("User not found");
+  }
+
+  return (
+    <>
+      <div className="my-6 flex flex-1 items-center justify-center">
+        <Challenges />
+      </div>
+    </>
+  );
+}
