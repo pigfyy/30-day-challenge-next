fe7cc9833fdc653a2c48e240af6e5d822a0c2085
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-02-24 17:19:24 -0800

    fix(actions.ts): update webpush VAPID details email format to include mailto for compliance with standards

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -7,7 +7,7 @@ import { Challenge } from "@prisma/client";
 import webpush from "web-push";
 
 webpush.setVapidDetails(
-  "franklinzhang06@gmail.com",
+  "mailto:franklinzhang06@gmail.com",
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!,
 );
