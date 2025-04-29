2d811e771785784cd73537eb658184bd2943cd0d
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-04-29 16:40:22 -0700

    fixed layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,19 +1,9 @@
-"use client";
+import { ReactNode } from "react";
 
-import { DevDialog } from "@/components/DevDialog";
-import { Header } from "@/components/Header";
-import { Toaster } from "@/components/ui/toaster";
-import { trpc, trpcClient } from "@/lib/util/trpc";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Suspense } from "react";
+type LayoutProps = {
+  children: ReactNode;
+};
 
-export const queryClient = new QueryClient();
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <main className="flex flex-1">{children}</main>
-    </>
-  );
+export default function AppLayout({ children }: LayoutProps) {
+  return <>{children}</>;
 }
