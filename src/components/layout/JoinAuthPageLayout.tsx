a5355eee64ea937d044de63a2d7d0191050483b3
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-05-20 20:31:17 -0700

    made in-app browser warning more prominent

--- src/components/layout/JoinAuthPageLayout.tsx
+++ src/components/layout/JoinAuthPageLayout.tsx
@@ -37,13 +37,43 @@ export default function JoinAuthPageLayout({
       <h1 className="text-3xl font-bold">Welcome!</h1>
       <p className="text-md mb-5 text-gray-500">{welcomeMessage}</p>
       {isInAppBrowserClient && (
-        <div className="mb-4 rounded bg-yellow-100 p-3 text-center text-yellow-700">
-          <p>
-            It looks like you&apos;re in an in-app browser. To be able to create
-            an account or use Sign In with Google, please either copy the link
-            and paste it into your browser or click on the top-right ellipsis
-            and select Open in Browser
-          </p>
+        <div className="animated-wavy-border mb-4 rounded p-0.5 text-center">
+          <style jsx>{`
+            @keyframes wavyBorderAnimation {
+              0% {
+                background-position: 0% 50%;
+              }
+              50% {
+                background-position: 100% 50%;
+              }
+              100% {
+                background-position: 0% 50%;
+              }
+            }
+            .animated-wavy-border {
+              background-image: linear-gradient(
+                to right,
+                #fcd34d,
+                /* amber-300 */ #fbbf24,
+                /* amber-400 */ #f59e0b,
+                /* amber-500 */ #d97706,
+                /* amber-600 */ #b45309,
+                /* amber-700 */ #f59e0b,
+                /* amber-500 */ #fbbf24,
+                /* amber-400 */ #fcd34d /* amber-300 */
+              );
+              background-size: 250% auto;
+              animation: wavyBorderAnimation 3s linear infinite;
+            }
+          `}</style>
+          <div className="rounded bg-yellow-200 p-2.5">
+            <p className="text-yellow-800">
+              It looks like you&apos;re in an in-app browser. To be able to
+              create an account or use Sign In with Google, please either copy
+              the link and paste it into your browser or click on the top-right
+              ellipsis and select Open in Browser
+            </p>
+          </div>
         </div>
       )}
       {type === "sign-in" ? (
