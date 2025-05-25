5e4c10085b474010c0456355dcea9de1ffd8b0ea
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-05-25 14:19:20 -0700

    updated admin ids

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,5 @@ export const EXCELLENT_PROGRESS_THRESHOLD = 0.8;
 export const ADMIN_IDS = [
   "user_2xNoK9NQhHtRIWU5Pi9D4MSolWI",
   "user_2xNeGvUdHkyRkS6ic4jvFiRPv6Q",
+  "user_2xNeAxeRhmVJpFMifqh7LrOgc9A",
 ];
