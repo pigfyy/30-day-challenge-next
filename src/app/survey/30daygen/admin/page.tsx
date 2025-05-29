49793f59adced48aa22ed2160a81b9556e93900d
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-05-28 21:40:05 -0700

    changed to use db instead of prodDb

--- src/app/survey/30daygen/admin/page.tsx
+++ src/app/survey/30daygen/admin/page.tsx
@@ -1,11 +1,11 @@
-import { prodDb } from "@/lib/db/drizzle";
+import { db, prodDb } from "@/lib/db/drizzle";
 import { validateAdmin } from "@/lib/util";
 import { SurveyAnalyticsDashboard } from "./(components)/SurveyAnalyticsDashboard";
 
 export default async function Survey30DayGenAdminPage() {
   await validateAdmin();
 
-  const responses = await prodDb.query.surveyResponse.findMany();
+  const responses = await db.query.surveyResponse.findMany();
 
   return (
     <div className="container mx-auto px-4 py-8">
